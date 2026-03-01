5d2cf925882ccb9c1a752f05bffbe00c0aac4c68
lylejasonsmal <161517208+lylejasonsmal@users.noreply.github.com>
2026-03-01 17:26:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Mugprintz-VueJs/',
   plugins: [
     vue()
   ],
